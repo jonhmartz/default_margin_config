e14d88b349b834e43a498f0108798e82a7ca640f
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-08 23:35:46 -0600

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,13 @@
+
 {
     "name": "Configuración de Utilidad por Defecto",
     "version": "1.0",
-    "category": "Sales",
-    "summary": "Permite configurar una utilidad por defecto para ventas",
-    "author": "Tu Empresa",
     "depends": ["base", "sale"],
+    "author": "Tu Empresa",
+    "category": "Sales",
     "data": [
         "security/ir.model.access.csv",
-        "views/margin_config_view.xml",
-        "views/sale_order_line_view.xml"
+        "views/margin_config_view.xml"
     ],
     "installable": True,
     "application": True,
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from . import default_margin_config, sale_order_line
+from . import default_margin_config
